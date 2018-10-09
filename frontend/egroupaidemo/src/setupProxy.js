610e7aa9802d3e3e1e8270f9224440c465c13c16
SP_Penguin <abrcdf1023@gmail.com>
2018-10-09 16:49:51 +0800

    fixed proxy

--- frontend/egroupaidemo/src/setupProxy.js
+++ frontend/egroupaidemo/src/setupProxy.js
@@ -1,5 +1,6 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(proxy('/websocket', { target: 'http://localhost:8080', ws: true }));
+  app.use(proxy('/websocket', { target: 'ws://10.211.55.3:8080', ws: true }));
+  app.use(proxy('/api', { target: 'http://10.211.55.3:8080' }));
 };
